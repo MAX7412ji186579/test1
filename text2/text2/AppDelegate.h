091b9827aa92f4f8a89fc1357f17470f74342c15
MAX7412ji186579 <1194665583@qq.com>
2016-06-07 10:51:30 +0800

    66666666

---
+++ text2/text2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  text2
+//
+//  Created by mac-max on 16/6/7.
+//  Copyright © 2016年 AiShuiShui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
